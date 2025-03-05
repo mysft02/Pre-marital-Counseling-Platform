21285e9654c720ad4ee2142cd86fab1af15af73b
tdat-le <tandatlxag1602@gmail.com>
2025-03-06 01:08:57 +0700

    CertificateController

---
+++ Pre-marital Counseling Platform/Controllers/CertificateController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SWP391.DTO.Certificate;
+using SWP391.Service;
+
+namespace SWP391.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class CertificateController : ControllerBase
+    {
+        private readonly ICertificateService _certificateService;
+
+        public CertificateController (ICertificateService certificateService)
+        {
+            _certificateService = certificateService;
+        }
+
+        [HttpGet("Get_All_Certificate")]
+        public async Task<IActionResult> GetAllCertificate()
+        {
+            return await _certificateService.GetAllCertificate();
+        }
+
+        [HttpGet("Get_Certificate_By_Id")]
+        public async Task<IActionResult> GetCertificateById(Guid id)
+        {
+            return await _certificateService.GetCertificateById(id);
+        }
+
+        [Authorize]
+        [HttpPost("Create_Certificate")]
+        public async Task<IActionResult> CreateCertificate([FromBody] CreateCertificateDTO dto)
+        {
+            var currentUser = HttpContext.User;
+            var userId = currentUser.FindFirst("UserId")?.Value;
+            return await _certificateService.CreateCertificate(dto, userId);
+        }
+
+        [Authorize]
+        [HttpPost("Update_Certificate")]
+        public async Task<IActionResult> UpdateCertificate([FromBody] UpdateCertificateDTO dto)
+        {
+            var currentUser = HttpContext.User;
+            var userId = currentUser.FindFirst("UserId")?.Value;
+            return await _certificateService.UpdateCertificate(dto, userId);
+        }
+    }
+}
---
+++ Pre-marital Counseling Platform/DTO/Certificate/CertificateDTO.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using SWP391.DTO.Certificate;
+
+namespace SWP391.DTO
+{
+    public class CertificateDTO
+    {
+        public Guid CertificateId { get; set; }
+        public Guid TherapistId { get; set; }
+        public string CertificateName { get; set; }
+        public string CertificateUrl { get; set; }
+    }
+
+    public class CertificateProfile : Profile
+    {
+        public CertificateProfile()
+        {
+            CreateMap<SWP391.Domain.Certificate, CertificateDTO>().ReverseMap();
+            CreateMap<CreateCertificateDTO, SWP391.Domain.Certificate>().ReverseMap();
+        }
+    }
+}
