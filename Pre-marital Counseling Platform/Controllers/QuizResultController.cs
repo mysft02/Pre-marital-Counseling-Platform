e32d68ad70843a2ab5c0df83b0f2388e53ba816b
tdat-le <tandatlxag1602@gmail.com>
2025-03-11 19:28:15 +0700

    QuizResult

---
+++ Pre-marital Counseling Platform/Controllers/QuizResultController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SWP391.Service;
+
+namespace SWP391.Controllers
+{
+    [Route("api/Controller")]
+    [ApiController]
+    public class QuizResultController : ControllerBase
+    {
+        private readonly IQuizService _service;
+
+        public QuizResultController(IQuizService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost("Create_List_Quiz_Result")]
+        public async Task<IActionResult> CreateQuizResult([FromBody] List<CreateListQuizResultDTO> dto)
+        {
+            var currentUser = HttpContext.User;
+            var userId = currentUser.FindFirst("UserId")?.Value;
+            return await _service.
+        }
+    }
+}
---
+++ Pre-marital Counseling Platform/Service/IQuizResultService.cs
@@ -0,0 +1,81 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using SWP391.Domain;
+using SWP391.DTO;
+using SWP391.Infrastructure.DbContext;
+
+namespace SWP391.Service
+{
+    public interface IQuizResultService
+    {
+        Task<IActionResult> CreateQuizResult(List<CreateQuizResultDTO> dto, string? userId);
+        Task<IActionResult> UpdateQuizResult(CreateQuizResultDTO dto, string? userId);
+    }
+
+    public class QuizResultService : ControllerBase, IQuizResultService
+    {
+
+        private readonly IMapper _mapper;
+        private readonly PmcsDbContext _context;
+
+        public QuizResultService(IMapper mapper, PmcsDbContext context)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        public async Task<IActionResult> CreateQuizResult(List<CreateQuizResultDTO> dto, string? userId)
+        {
+            try
+            {
+                if(userId == null)
+                {
+                    return BadRequest("User ID is required");
+                }
+
+                foreach (var item in dto) {
+                    var quizResult = _mapper.Map<QuizResult>(item);
+                    quizResult.CreatedBy = Guid.Parse(userId);
+                    quizResult.CreatedAt = DateTime.Now;
+                    quizResult.UpdatedBy = Guid.Parse(userId);
+                    quizResult.UpdatedAt = DateTime.Now;
+                    _context.Add(quizResult);
+                }
+                
+                var rs = await _context.SaveChangesAsync();
+                if(rs > 0)
+                {
+                    return Ok("Create successfully");
+                }
+                else
+                {
+                    return BadRequest("Create failed");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        public async Task<IActionResult> UpdateQuizResult(CreateQuizResultDTO dto, string? userId)
+        {
+            try
+            {
+                var quizResult = _mapper.Map<QuizResult>(dto);
+                _context.QuizResults.Update(quizResult);
+                if (_context.SaveChanges() > 0)
+                {
+                    return Ok("Update successfully");
+                }
+                else
+                {
+                    return BadRequest("Update failed");
+                }
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
